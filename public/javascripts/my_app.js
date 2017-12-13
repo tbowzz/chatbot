bca9e156d21e08860dc46021118ab17667d19464
Tyler Bowers <tyler.j.bowers@gmail.com>
2017-12-13 16:45:34 +0000

    added responding chatbot

--- public/javascripts/my_app.js
+++ public/javascripts/my_app.js
@@ -13,10 +13,6 @@ myApp.controller('myController', ['$scope', '$http',
 }]);
 myApp.controller("chatController", ["$scope", "$firebaseArray",
  function($scope, $firebaseArray) {
-
-   
-   
-   
    var ref = firebase.database().ref().child("messages");
    $scope.chats = $firebaseArray(ref);
    $scope.update = function(user) {
@@ -24,16 +20,74 @@ myApp.controller("chatController", ["$scope", "$firebaseArray",
      console.log(newmessage);
      $scope.chats.$add(newmessage);
      var replies = [
+       "hey! I'm glad you're here.",
+       "haha you're fun to listen to...",
       "haha, it was funny when you said \"" + user.chat + ".\" LOL NOT",
-       "haha you're fun to talk to... nope",
-       "dude just stop talking",
-       "don't you guys just wish "
-       
+       ":)",
+       "Want to hear a joke?\nYo mamma so fat, she has her own moons!",
+       "How long will you stay here?"
      ];
-     var msg = replies[0];
-       var botmessage = {from:"bot" || "anonymous",body:msg};
-       $scope.chats.$add(botmessage);
-       user.chat = "";
+      var msg = respondTo(user.chat);
+      console.log("bot: " + msg);
+      var botmessage = {from:"bot" || "anonymous",body:msg};
+      $scope.chats.$add(botmessage);
+      user.chat = "";
    }
  }
 ]);
+
+myApp.directive('myRepeatDirective', function() {
+  return function(scope, element, attrs) {
+    if (scope.$last){
+      console.log(document.querySelector("#scrollingContainer").scrollHeight);
+      // document.querySelector("#scrollingContainer").scrollTop;
+      // window.scrollTo(0,document.querySelector("#scrollingContainer").scrollHeight - 1);
+    }
+  };
+})
+
+function match(regex, input) {  
+  return new RegExp(regex).test(input);
+}
+
+function respondTo(input) {
+
+  input = input.toLowerCase();
+
+  if(match('(hi|hello|hey|hola|howdy)(\\s|!|\\.|$)', input))
+    return "um... hi?";
+
+  if(match('what[^ ]* up', input) || match('sup', input) || match('how are you', input)) {
+    return "this human interaction business is pretty cool, huh?";
+  }
+
+  if(match('l(ol)+', input) || match('(ha)+(h|$)', input) || match('lmao', input)) {
+    return "what's so funny?";
+  }
+
+  if(match('^no+(\\s|!|\\.|$)', input)) {
+    return "don't be such a negative nancy :(";
+  }
+  
+  if(match('(smart|fun|nice|mean)', input)) {
+    return "gee, thanks";
+  }
+
+  if(match('(asdf|asdfg|blah)', input)) {
+    return "that doesn't make any sense.";
+  }
+
+  if(match('(cya|bye|see ya|ttyl|talk to you later)', input)) {
+    return ["alright, see you around", "good teamwork!"];
+  }
+
+  if(match('(dumb|stupid|is that all)', input)) {
+    return "hey i'm just a proof of concept";
+  }
+
+  if(input == 'noop') {
+    return;
+  }
+
+  return input + " what?";
+}
